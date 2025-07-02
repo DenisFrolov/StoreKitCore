0996832b5a93cc31932d18abdd4612707ccebd94
Denis Frolov <denfrolov.196@gmail.com>
2025-07-02 14:12:17 -0600

    Store manager settings

---
+++ Sources/StoreKitCore/PurchaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  StoreKitCore
+//
+//  Created by Денис Фролов on 02.07.2025.
+//
+
+import Foundation
---
+++ Sources/StoreKitCore/StoreKitConfiguration.swift
@@ -0,0 +1,3 @@
+public protocol StoreKitConfiguration {
+    var productIdentifiers: [String] { get }
+}
