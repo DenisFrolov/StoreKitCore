70fb45972d882a849959ce701ca0b33f8c4ea8a5
Denis Frolov <denfrolov.196@gmail.com>
2025-07-04 10:34:11 -0600

    Added extensions

--- Sources/StoreKitCore/Product+Extensions.swift
+++ Sources/StoreKitCore/Product+Extensions.swift
@@ -29,4 +29,33 @@ extension Product {
             return nil
         }
     }
+
+    public var weekPrice: Decimal? {
+        if #available(iOS 16.4, *) {
+            switch subscription?.subscriptionPeriod {
+            case .everyThreeDays:   return nil
+            case .weekly:           return nil
+            case .everyTwoWeeks:    return price / 2
+            case .monthly:          return price / 4
+            case .everyTwoMonths:   return price / 8
+            case .everyThreeMonths: return price / 12
+            case .everySixMonths:   return price / 24
+            case .yearly:           return price / 48
+            case .none:             return nil
+            case .some(_):          return nil
+            }
+        } else {
+            return nil
+        }
+    }
+
+    public var weekPriceFormatted: String? {
+        guard let weekPrice = weekPrice else { return nil }
+        return priceFormatStyle.format(weekPrice)
+    }
+
+    public var promoPeriod: String? {
+        guard let intro =  subscription?.introductoryOffer else { return nil }
+        return intro.period.formatted(subscriptionPeriodFormatStyle, referenceDate: .now)
+    }
 }
